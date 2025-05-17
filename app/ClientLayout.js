bb8594893266121ac0a414303de71dfcf1709c74
somacruzlistic <rotuber999@gmail.com>
2025-05-17 17:43:07 +0530

    chore: remove all files from git tracking

--- app/ClientLayout.js
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { SessionProvider } from 'next-auth/react';
-
-export default function ClientLayout({ children }) {
-  console.log('ClientLayout rendering with SessionProvider');
-  return <SessionProvider>{children}</SessionProvider>;
-}
